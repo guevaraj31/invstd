6f1e8e4b40e7f96f960cf1851934af2662412e70
Jesus Guevara <guevaraj31@gmail.com>
2020-07-18 12:40:54 -0500

    Migracion para objeto producto

--- database/migrations/2020_07_18_172147_create_products_table.php
+++ database/migrations/2020_07_18_172147_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('sku');
+            $table->integer('qty');
+            $table->float('price', 9, 2);
             $table->timestamps();
         });
     }
